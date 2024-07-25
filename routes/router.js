e2173132946d662b4d932f705a1937565d68f73e
funkim <Mcking172@skiff.com>
2024-07-25 17:41:31 -0400

    Log out added.

--- routes/router.js
+++ routes/router.js
@@ -26,6 +26,21 @@ router.post(
   })
 );
 
+router.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.session.destroy((err) => {
+      if (err) {
+        console.log('Error : Failed to destroy the session during logout.', err);
+      }
+      req.user = null;
+      res.redirect('/');
+    });
+  });
+});
+
 router.get('/register', (req, res) => {
   res.render('register');
 });
