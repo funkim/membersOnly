9c65f875f1acc2e6295ad7e752f897357b6e7351
funkim <Mcking172@skiff.com>
2024-07-25 11:04:52 -0400

    Register / Login functionality

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.use('/', router);
-
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use('/', router);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Express listening on port ${PORT}`));
--- routes/router.js
+++ routes/router.js
@@ -3,16 +3,25 @@ const router = express.Router();
 const passport = require('passport');
 const passwordUtils = require('../lib/passwordUtils');
 const db = require('../db/data');
+const session = require('express-session');
 
 router.get('/', (req, res) => res.render('index'));
 
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 router.post(
   '/login',
   passport.authenticate('local', {
     failureRedirect: '/login-failure',
     successRedirect: '/login-success',
+    session: session,
   })
 );
+
+router.get('/register', (req, res) => {
+  res.render('register');
+});
 
 router.post('/register', async (req, res, next) => {
   const { username, password, membership } = req.body;
@@ -24,7 +33,14 @@ router.post('/register', async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-  res.render('register');
+});
+
+router.get('/login-success', (req, res, next) => {
+  res.send('<p>You successfully logged in. --> <a href="/protected-route">Go to protected route</a></p>');
+});
+
+router.get('/login-failure', (req, res, next) => {
+  res.send('You entered the wrong password.');
 });
 
 module.exports = router;
